fd980890598ffb09c454887c09f2b3cc99c66839
Javid Sadigli <cavidoffical@gmail.com>
2024-04-03 23:48:03 +0400

    Cleared App.java

--- src/App.java
+++ src/App.java
@@ -4,10 +4,6 @@ public class App
 {
     public static void main(String[] args) 
     {
-        Student student = new Student("Qumru Sadigli", 1);
-        student.save();
-        for (int i = 0; i < Student.getAll().length; i++) {
-            System.out.println(Student.getAll()[i].getFullName());
-        }
+        
     }
 }
