e8d0ee11a15e04da2056579d65f6cd6533959c68
Javid Sadigli <cavidoffical@gmail.com>
2024-04-13 02:42:12 +0400

    Some changes on database path

--- src/toolkit/DatabasePath.java
+++ src/toolkit/DatabasePath.java
@@ -7,20 +7,9 @@ public class DatabasePath {
     public static String getDatabasePath(String databaseFileName) {
         // Get the current directory of the project
         Path currentDir = Paths.get("").toAbsolutePath();
-
-        // Check the operating system
-        String os = System.getProperty("os.name").toLowerCase();
         Path relativePath;
-        if (os.contains("win")) {
-            // Windows path
-            relativePath = Paths.get("database", databaseFileName);
-        } else if (os.contains("mac") || os.contains("nix") || os.contains("nux") || os.contains("aix")) {
-            // MacOS, Linux, Unix-like path
-            relativePath = Paths.get("database", databaseFileName);
-        } else {
-            // Default path (you can customize this for other OS)
-            relativePath = Paths.get("database", databaseFileName);
-        }
+
+        relativePath = Paths.get("database", databaseFileName);
 
         // Combine the current directory with the relative path
         Path databasePath = currentDir.resolve(relativePath);
