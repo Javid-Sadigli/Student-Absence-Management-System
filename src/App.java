bc9e089650867d554692c629921af9d03446ba72
Javid Sadigli <cavidoffical@gmail.com>
2024-04-12 23:42:02 +0400

    Some changes on constructors

--- src/App.java
+++ src/App.java
@@ -1,13 +1,10 @@
+import models.Group;
 import models.Student;
 
 public class App 
 {
     public static void main(String[] args) 
     {
-        Student[] students = Student.filterByGroup(1); 
-        for (Student student : students)
-        {
-            System.out.println(student.getFullName());
-        }
+        Student[] students = Student.getAll();
     }
 }
