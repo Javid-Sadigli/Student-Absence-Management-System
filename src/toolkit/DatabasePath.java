ad4549ad827fd998fd466343ff204fdc36499fab
Javid Sadigli <cavidoffical@gmail.com>
2024-04-14 20:21:54 +0400

    Javadoc comments for Absent class have been added

--- src/toolkit/DatabasePath.java
+++ src/toolkit/DatabasePath.java
@@ -2,7 +2,16 @@ package toolkit;
 
 import java.nio.file.Paths;
 
+/**
+ *  A database path class to show the correct path of database for saving objects.
+ */
 public class DatabasePath {
+
+    /**
+     * Gives the path of the database file for saving objects.
+     * @param modelName The name of the Model to which the object to be saved belongs.
+     * @return The path of the database file for saving the object whose modelName is given.
+     */
     public static String getDatabasePath(String modelName) {
         // Get the current directory of the project
         String currentDir = Paths.get("").toAbsolutePath().toString();
